4ec558f4dc6e9ac09b18ad1c130734e7ec3b0aa5
Renan Reschke <renanreschke@hotmail.com>
2021-04-13 22:10:14 -0300

    Compactação dos audios e inicio do main

--- Trabalhos/T2/Parte-2/main.m
+++ Trabalhos/T2/Parte-2/main.m
@@ -16,7 +16,7 @@
 %%  g. Verificar qualidade do sistema de aprendizado para cada vogal
 %%
 %%
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%Limpeza
 
@@ -31,21 +31,21 @@ warning('off')
 
 %%Inicializacao do programa
 disp('Iniciando o programa...')
-fprintf('\nSinal analisado: o3.wav\n');
+fprintf('\nSinal analisado: a1.wav\n');
 fprintf('\nTipo: Discreto\n');
 fprintf('\nRecuperando dados do sinal\n');
 
 %%Recuperacao de dados da amostra
-[gk, fs] = audioread ('audio/o3.wav');         % transforma um arquivo .wav em um vetor g(k)
-                                        % recupera a taxa de amostragem - fs
-N       = length(gk);                   % numero de pontos do sinal em analise
-Ts      = 1/fs;                         % tempo de amostragem
-ws      = 2*pi*fs;                      % frequencia anngular
-duracao = N*Ts;                         % Duracao do sinal
-tempo   = linspace(0,duracao,N);        % vetor tempo computacional
-fmax    = fs/2;                         % frequencia maxima de amostragem
-frequencia = linspace(-fmax,fmax,N);    % vetor de frequencias de Fourier
-resolucao  = fs/N;                      % resolucao em frequencia
+[gk, fs] = audioread ('audio/a1.wav');          % transforma um arquivo .wav em um vetor g(k)
+                                                % recupera a taxa de amostragem - fs
+N       = length(gk);                           % numero de pontos do sinal em analise
+Ts      = 1/fs;                                 % tempo de amostragem
+ws      = 2*pi*fs;                              % frequencia anngular
+duracao = N*Ts;                                 % Duracao do sinal
+tempo   = linspace(0,duracao,N);                % vetor tempo computacional
+fmax    = fs/2;                                 % frequencia maxima de amostragem
+frequencia = linspace(-fmax,fmax,N);            % vetor de frequencias de Fourier
+resolucao  = fs/N;                              % resolucao em frequencia
 fprintf('Tempo de duracao: %.3fs\n', duracao);
 
 
@@ -55,7 +55,7 @@ wn                = exp(-1i*2*pi/N);                  % Definindo wn
 Matriz_jotas      = wn*ones(N, N);                    % Matriz NxN wn
 Matriz_expoentes  = [0:1:N-1]'*[0:1:N-1];              
 Wn                = Matriz_jotas.^Matriz_expoentes;   %
-Xn                = Wn*gk;
+Xn                = Wn*gk;                            % Calculo da matriz Xn
 
 fprintf('Duracao da analise por produto matricial: ');
 toc;
@@ -66,15 +66,15 @@ fig1 = figure(1);
 plot(tempo,gk,'k-')                   % configura plot(x,y, cor preta)
 xlabel('Tempo em segundos')           % tempo em segundos
 ylabel('Amplitude')                   % amplitude
-title('Sinal o3.wav amostrado')        % titulo
+title('Sinal a1.wav amostrado')       % titulo
 grid
-saveas(fig1,'o3.png') 
+saveas(fig1,'a1.png')                 % Salva o grafico como png
 
 fig2 = figure(2);
-plot(frequencia,fftshift(log10(abs(Xn))),'k-')         % configura plot(x,y, cor preta)
-xlabel('Frequencia em Hz')            % tempo em segundos
-ylabel('Amplitude')                   % amplitude em volts
-title('Espectro de amplitude de o3.wav')        % titulo
+plot(frequencia,fftshift(log10(abs(Xn))),'k-')          % configura plot(x,y, cor preta)
+xlabel('Frequencia em Hz')                              % tempo em segundos
+ylabel('Amplitude')                                     % amplitude em volts
+title('Espectro de amplitude de a1.wav')                % titulo
 grid
-saveas(fig2,'freq_o3.png')            %Salva os graficos como png
+saveas(fig2,'freq_a1.png')                              % Salva o grafico como png
          
